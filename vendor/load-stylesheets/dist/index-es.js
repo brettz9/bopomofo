7fcff5019d98bf2ec66fadb127424b505e909a09
Brett Zamir <brettz9@yahoo.com>
2024-06-20 16:35:55 +0800

    chore: update devDeps.

--- vendor/load-stylesheets/dist/index-es.js
+++ vendor/load-stylesheets/dist/index-es.js
@@ -1,38 +1,36 @@
+function _iterableToArrayLimit(r, l) {
+  var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"];
+  if (null != t) {
+    var e,
+      n,
+      i,
+      u,
+      a = [],
+      f = !0,
+      o = !1;
+    try {
+      if (i = (t = t.call(r)).next, 0 === l) {
+        if (Object(t) !== t) return;
+        f = !1;
+      } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0);
+    } catch (r) {
+      o = !0, n = r;
+    } finally {
+      try {
+        if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return;
+      } finally {
+        if (o) throw n;
+      }
+    }
+    return a;
+  }
+}
 function _slicedToArray(arr, i) {
   return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();
 }
-
 function _arrayWithHoles(arr) {
   if (Array.isArray(arr)) return arr;
 }
-
-function _iterableToArrayLimit(arr, i) {
-  if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;
-  var _arr = [];
-  var _n = true;
-  var _d = false;
-  var _e = undefined;
-
-  try {
-    for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
-      _arr.push(_s.value);
-
-      if (i && _arr.length === i) break;
-    }
-  } catch (err) {
-    _d = true;
-    _e = err;
-  } finally {
-    try {
-      if (!_n && _i["return"] != null) _i["return"]();
-    } finally {
-      if (_d) throw _e;
-    }
-  }
-
-  return _arr;
-}
-
 function _unsupportedIterableToArray(o, minLen) {
   if (!o) return;
   if (typeof o === "string") return _arrayLikeToArray(o, minLen);
@@ -41,72 +39,98 @@ function _unsupportedIterableToArray(o, minLen) {
   if (n === "Map" || n === "Set") return Array.from(o);
   if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
 }
-
 function _arrayLikeToArray(arr, len) {
   if (len == null || len > arr.length) len = arr.length;
-
   for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
-
   return arr2;
 }
-
 function _nonIterableRest() {
   throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
 }
 
+/**
+ * @param {string|string[]} stylesheets
+ * @param {{
+ *   before?: HTMLElement,
+ *   after?: HTMLElement,
+ *   favicon?: boolean,
+ *   image?: boolean,
+ *   canvas?: boolean,
+ *   acceptErrors?: boolean|((info: {
+ *     error: ErrorEvent,
+ *     stylesheetURL: string,
+ *     options: {},
+ *     resolve: (value: any) => void,
+ *     reject: (reason?: any) => void
+ *   }) => (reason?: any) => void)
+ * }} cfg
+ * @returns {Promise<HTMLLinkElement[]>}
+ */
 function loadStylesheets(stylesheets) {
   var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-      beforeDefault = _ref.before,
-      afterDefault = _ref.after,
-      faviconDefault = _ref.favicon,
-      canvasDefault = _ref.canvas,
-      _ref$image = _ref.image,
-      imageDefault = _ref$image === void 0 ? true : _ref$image,
-      acceptErrors = _ref.acceptErrors;
-
+    beforeDefault = _ref.before,
+    afterDefault = _ref.after,
+    faviconDefault = _ref.favicon,
+    canvasDefault = _ref.canvas,
+    _ref$image = _ref.image,
+    imageDefault = _ref$image === void 0 ? true : _ref$image,
+    acceptErrors = _ref.acceptErrors;
   stylesheets = Array.isArray(stylesheets) ? stylesheets : [stylesheets];
 
-  function setupLink(stylesheetURL) {
+  /**
+   * @typedef {{
+   *   before?: HTMLElement,
+   *   after?: HTMLElement,
+   *   favicon?: boolean,
+   *   image?: boolean,
+   *   canvas?: boolean,
+   * }} Options
+   */
+
+  /**
+   * @param {string|[stylesheetURL: string, options: Options]} stylesheetURLInfo
+   * @returns {Promise<HTMLLinkElement>}
+   */
+  function setupLink(stylesheetURLInfo) {
+    /** @type {Options} */
     var options = {};
 
-    if (Array.isArray(stylesheetURL)) {
-      var _stylesheetURL = stylesheetURL;
-
-      var _stylesheetURL2 = _slicedToArray(_stylesheetURL, 2);
-
-      stylesheetURL = _stylesheetURL2[0];
-      var _stylesheetURL2$ = _stylesheetURL2[1];
-      options = _stylesheetURL2$ === void 0 ? {} : _stylesheetURL2$;
+    /** @type {string} */
+    var stylesheetURL;
+    if (Array.isArray(stylesheetURLInfo)) {
+      var _stylesheetURLInfo = _slicedToArray(stylesheetURLInfo, 2);
+      stylesheetURL = _stylesheetURLInfo[0];
+      var _stylesheetURLInfo$ = _stylesheetURLInfo[1];
+      options = _stylesheetURLInfo$ === void 0 ? {} : _stylesheetURLInfo$;
+    } else {
+      stylesheetURL = stylesheetURLInfo;
     }
-
     var _options = options,
-        _options$favicon = _options.favicon,
-        favicon = _options$favicon === void 0 ? faviconDefault : _options$favicon;
+      _options$favicon = _options.favicon,
+      favicon = _options$favicon === void 0 ? faviconDefault : _options$favicon;
     var _options2 = options,
-        _options2$before = _options2.before,
-        before = _options2$before === void 0 ? beforeDefault : _options2$before,
-        _options2$after = _options2.after,
-        after = _options2$after === void 0 ? afterDefault : _options2$after,
-        _options2$canvas = _options2.canvas,
-        canvas = _options2$canvas === void 0 ? canvasDefault : _options2$canvas,
-        _options2$image = _options2.image,
-        image = _options2$image === void 0 ? imageDefault : _options2$image;
-
+      _options2$before = _options2.before,
+      before = _options2$before === void 0 ? beforeDefault : _options2$before,
+      _options2$after = _options2.after,
+      after = _options2$after === void 0 ? afterDefault : _options2$after,
+      _options2$canvas = _options2.canvas,
+      canvas = _options2$canvas === void 0 ? canvasDefault : _options2$canvas,
+      _options2$image = _options2.image,
+      image = _options2$image === void 0 ? imageDefault : _options2$image;
     function addLink() {
       if (before) {
         before.before(link);
       } else if (after) {
         after.after(link);
       } else {
-        document.head.appendChild(link);
+        document.head.append(link);
       }
     }
+    var link = document.createElement('link');
 
-    var link = document.createElement('link'); // eslint-disable-next-line promise/avoid-new -- No native option
-
+    // eslint-disable-next-line promise/avoid-new -- No native option
     return new Promise(function (resolve, reject) {
       var rej = reject;
-
       if (acceptErrors) {
         rej = typeof acceptErrors === 'function' ? function (error) {
           acceptErrors({
@@ -118,34 +142,33 @@ function loadStylesheets(stylesheets) {
           });
         } : resolve;
       }
-
       if (stylesheetURL.endsWith('.css')) {
         favicon = false;
       } else if (stylesheetURL.endsWith('.ico')) {
         favicon = true;
       }
-
       if (favicon) {
         link.rel = 'shortcut icon';
         link.type = 'image/x-icon';
-
         if (image === false) {
           link.href = stylesheetURL;
           addLink();
           resolve(link);
           return;
         }
-
         var cnv = document.createElement('canvas');
         cnv.width = 16;
         cnv.height = 16;
         var context = cnv.getContext('2d');
-        var img = document.createElement('img'); // eslint-disable-next-line promise/prefer-await-to-callbacks -- No API
-
+        var img = document.createElement('img');
+        // eslint-disable-next-line promise/prefer-await-to-callbacks -- No API
         img.addEventListener('error', function (error) {
           reject(error);
         });
         img.addEventListener('load', function () {
+          if (!context) {
+            throw new Error('Canvas context could not be found');
+          }
           context.drawImage(img, 0, 0);
           link.href = canvas ? cnv.toDataURL('image/x-icon') : stylesheetURL;
           addLink();
@@ -154,12 +177,11 @@ function loadStylesheets(stylesheets) {
         img.src = stylesheetURL;
         return;
       }
-
       link.rel = 'stylesheet';
       link.type = 'text/css';
       link.href = stylesheetURL;
-      addLink(); // eslint-disable-next-line promise/prefer-await-to-callbacks -- No API
-
+      addLink();
+      // eslint-disable-next-line promise/prefer-await-to-callbacks -- No API
       link.addEventListener('error', function (error) {
         rej(error);
       });
@@ -168,10 +190,9 @@ function loadStylesheets(stylesheets) {
       });
     });
   }
-
   return Promise.all(stylesheets.map(function (stylesheetURL) {
     return setupLink(stylesheetURL);
   }));
 }
 
-export default loadStylesheets;
+export { loadStylesheets as default };
--- vendor/simple-prefs/index.esm.js
+++ vendor/simple-prefs/index.esm.js
@@ -3,106 +3,111 @@ function _classCallCheck(instance, Constructor) {
     throw new TypeError("Cannot call a class as a function");
   }
 }
-
 function _defineProperties(target, props) {
   for (var i = 0; i < props.length; i++) {
     var descriptor = props[i];
     descriptor.enumerable = descriptor.enumerable || false;
     descriptor.configurable = true;
     if ("value" in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
+    Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor);
   }
 }
-
 function _createClass(Constructor, protoProps, staticProps) {
   if (protoProps) _defineProperties(Constructor.prototype, protoProps);
   if (staticProps) _defineProperties(Constructor, staticProps);
+  Object.defineProperty(Constructor, "prototype", {
+    writable: false
+  });
   return Constructor;
+}
+function _toPrimitive(input, hint) {
+  if (typeof input !== "object" || input === null) return input;
+  var prim = input[Symbol.toPrimitive];
+  if (prim !== undefined) {
+    var res = prim.call(input, hint || "default");
+    if (typeof res !== "object") return res;
+    throw new TypeError("@@toPrimitive must return a primitive value.");
+  }
+  return (hint === "string" ? String : Number)(input);
+}
+function _toPropertyKey(arg) {
+  var key = _toPrimitive(arg, "string");
+  return typeof key === "symbol" ? key : String(key);
 }
 
 /**
- * @module SimplePrefs
- */
-
-/**
-* @typedef {PlainObject<{
-* string: module:SimplePrefs.Value}>} module:SimplePrefs.Defaults
+* @typedef {{[key: string]: Value}} Defaults
 */
 
 /**
-* @typedef {boolean|number|string} module:SimplePrefs.Value
+* @typedef {boolean|number|string} Value
 */
 
 /**
  * Preferences storage.
  */
+
 function _await(value, then, direct) {
   if (direct) {
     return then ? then(value) : value;
   }
-
   if (!value || !value.then) {
     value = Promise.resolve(value);
   }
-
   return then ? value.then(then) : value;
 }
+
 /**
  * Defaults for SimplePrefs.
  */
-
-
 var SimplePrefs = /*#__PURE__*/function () {
   /**
-   * @param {PlainObject} cfg
-   * @param {string} cfg.namespace Avoid clashes with other apps
-   * @param {module:SimplePrefs.Defaults} cfg.defaults
-   * @param {module:SimplePrefs.SimplePrefsDefaults} cfg.prefDefaults
-   * @returns {void}
+   * @param {object} cfg
+   * @param {string} [cfg.namespace] Avoid clashes with other apps
+   * @param {Defaults} [cfg.defaults]
+   * @param {SimplePrefsDefaults} [cfg.prefDefaults]
    */
   function SimplePrefs(cfg) {
     _classCallCheck(this, SimplePrefs);
-
     this.configurePrefs(cfg);
+
+    /** @type {((e: StorageEvent) => void)[]} */
     this.listeners = [];
   }
   /**
-   * @param {PlainObject} cfg
-   * @param {string} cfg.namespace Avoid clashes with other apps
-   * @param {module:SimplePrefs.Defaults} cfg.defaults
-   * @param {module:SimplePrefs.SimplePrefsDefaults} cfg.prefDefaults
+   * @param {object} cfg
+   * @param {string} [cfg.namespace] Avoid clashes with other apps
+   * @param {Defaults} [cfg.defaults]
+   * @param {SimplePrefsDefaults} [cfg.prefDefaults]
    * @returns {void}
    */
-
-
   _createClass(SimplePrefs, [{
     key: "configurePrefs",
     value: function configurePrefs(_ref) {
       var namespace = _ref.namespace,
-          defaults = _ref.defaults,
-          _ref$prefDefaults = _ref.prefDefaults,
-          prefDefaults = _ref$prefDefaults === void 0 ? simplePrefsDefaults(defaults) : _ref$prefDefaults;
-      Object.assign(this, {
-        namespace: namespace,
-        prefDefaults: prefDefaults
-      });
+        defaults = _ref.defaults,
+        _ref$prefDefaults = _ref.prefDefaults,
+        prefDefaults = _ref$prefDefaults === void 0 ? simplePrefsDefaults(defaults) : _ref$prefDefaults;
+      this.namespace = namespace !== null && namespace !== void 0 ? namespace : '';
+      this.prefDefaults = prefDefaults;
     }
     /**
      * Get parsed preference value; returns `Promise` in anticipation
      * of https://domenic.github.io/async-local-storage/ .
+     * @callback GetPref
      * @param {string} key Preference key (for Chrome-Compatibility, only `\w+`)
-     * @returns {Promise<module:SimplePrefs.Value>} Resolves to the parsed
+     * @returns {Promise<Value>} Resolves to the parsed
      *   value (defaulting if necessary)
      */
 
+    /** @type {GetPref} */
   }, {
     key: "getPref",
     value: function getPref(key) {
       try {
-        var _this2 = this;
-
-        var result = localStorage.getItem(_this2.namespace + key);
-        return _await(result === null ? _this2.prefDefaults.getPrefDefault(key) : JSON.parse(result), void 0, !(result === null));
+        var _this = this;
+        var result = localStorage.getItem(_this.namespace + key);
+        return _await(_await(result === null ? /** @type {SimplePrefsDefaults} */_this.prefDefaults.getPrefDefault(key) : JSON.parse(result), void 0, !(result === null)));
       } catch (e) {
         return Promise.reject(e);
       }
@@ -110,35 +115,33 @@ var SimplePrefs = /*#__PURE__*/function () {
     /**
      * Set a stringifiable preference value; returns `Promise` in anticipation
      *   of https://domenic.github.io/async-local-storage/ .
+     * @callback SetPref
      * @param {string} key Preference key (for Chrome-Compatibility, only `\w+`)
-     * @param {module:SimplePrefs.Value} val Stringifiable value
+     * @param {Value} val Stringifiable value
      * @returns {Promise<void>} Resolves after setting the item (Not currently
      *    in use)
      */
-
+    /** @type {SetPref} */
   }, {
     key: "setPref",
     value: function setPref(key, val) {
       try {
-        var _this4 = this;
-
-        return _await(localStorage.setItem(_this4.namespace + key, JSON.stringify(val)));
+        var _this2 = this;
+        return _await(localStorage.setItem(_this2.namespace + key, JSON.stringify(val)));
       } catch (e) {
         return Promise.reject(e);
       }
     }
     /**
-    * @typedef {PlainObject} GetPrefSetPref
-    * @property {module:SimplePrefs.SimplePrefs#getPref} getPref
-    * @property {module:SimplePrefs.SimplePrefs#setPref} setPref
+    * @typedef {object} GetPrefSetPref
+    * @property {GetPref} getPref
+    * @property {SetPref} setPref
     */
-
     /**
      * Convenience utility to return two main methods `getPref` and
      *   `setPref` bound to the current object.
      * @returns {GetPrefSetPref}
      */
-
   }, {
     key: "bind",
     value: function bind() {
@@ -147,29 +150,31 @@ var SimplePrefs = /*#__PURE__*/function () {
         setPref: this.setPref.bind(this)
       };
     }
+
     /**
     * @callback PreferenceCallback
+    * @param {StorageEvent} e
     * @returns {void}
     */
 
     /* eslint-disable promise/prefer-await-to-callbacks -- Repeating event */
-
     /**
-    * @param {string} [key]
+    * @param {string|PreferenceCallback|undefined} key
     * @param {PreferenceCallback} cb
-    * @returns {void}
+    * @returns {PreferenceCallback}
     */
-
   }, {
     key: "listen",
     value: function listen(key, cb) {
-      var _this5 = this;
-
+      var _this3 = this;
       if (typeof key === 'function') {
         cb = key;
         key = undefined;
       }
 
+      /**
+       * @param {StorageEvent} e
+       */
       var listener = function listener(e) {
         if (e.key === null) {
           // `null` for clear browser action or user `clear()`
@@ -178,27 +183,24 @@ var SimplePrefs = /*#__PURE__*/function () {
             return;
           }
         } else {
-          if (!e.key.startsWith(_this5.namespace)) {
+          if (!e.key.startsWith( /** @type {string} */_this3.namespace)) {
             return;
           }
-
-          if (key !== undefined && !e.key.startsWith(_this5.namespace + key)) {
+          if (key !== undefined && !e.key.startsWith( /** @type {string} */_this3.namespace + key)) {
             return;
           }
         }
-
         cb(e);
       };
-
       window.addEventListener('storage', listener);
       this.listeners.push(listener);
       return listener;
     }
+
     /**
      * @param {EventListener} listener
      * @returns {void}
      */
-
   }, {
     key: "unlisten",
     value: function unlisten(listener) {
@@ -211,43 +213,37 @@ var SimplePrefs = /*#__PURE__*/function () {
           }
         }
       }
-
       this.listeners.forEach(function (listenerItem) {
         window.removeEventListener('storage', listenerItem);
       });
     }
     /* eslint-enable promise/prefer-await-to-callbacks -- Repeating event */
-
   }]);
-
   return SimplePrefs;
 }();
 var SimplePrefsDefaults = /*#__PURE__*/function () {
   /**
    *
-   * @param {module:SimplePrefs.Defaults} defaults
+   * @param {{defaults: Defaults}} defaults
    */
   function SimplePrefsDefaults(_ref2) {
     var defaults = _ref2.defaults;
-
     _classCallCheck(this, SimplePrefsDefaults);
-
     this.defaults = defaults;
   }
   /**
    * Get parsed default value for a preference.
    * @param {string} key Preference key
-   * @returns {Promise<module:SimplePrefs.Value>}
+   * @returns {Promise<Value>}
    */
-
-
   _createClass(SimplePrefsDefaults, [{
     key: "getPrefDefault",
     value: function getPrefDefault(key) {
       try {
-        var _this7 = this;
-
-        return _await(_this7.defaults[key]);
+        var _this4 = this;
+        return _await(_this4.defaults[key], function (_this4$defaults$key) {
+          return _this4$defaults$key !== null && _this4$defaults$key !== void 0 ? _this4$defaults$key : null;
+        });
       } catch (e) {
         return Promise.reject(e);
       }
@@ -255,34 +251,33 @@ var SimplePrefsDefaults = /*#__PURE__*/function () {
     /**
      * Set parsed default value for a preference.
      * @param {string} key Preference key
-     * @param {module:SimplePrefs.Value} value
-     * @returns {Promise<module:SimplePrefs.Value>} The old value
+     * @param {Value} value
+     * @returns {Promise<Value>} The old value
      */
-
   }, {
     key: "setPrefDefault",
     value: function setPrefDefault(key, value) {
       try {
-        var _this9 = this;
-
-        var oldValue = _this9.defaults[key];
-        _this9.defaults[key] = value;
+        var _this5$defaults$key;
+        var _this5 = this;
+        var oldValue = (_this5$defaults$key = _this5.defaults[key]) !== null && _this5$defaults$key !== void 0 ? _this5$defaults$key : null;
+        _this5.defaults[key] = value;
         return _await(oldValue);
       } catch (e) {
         return Promise.reject(e);
       }
     }
   }]);
-
   return SimplePrefsDefaults;
 }();
+
 /**
  * Simplified factory for `SimplePrefsDefaults`
- * @param {module:SimplePrefs.Defaults} defaults
- * @returns {module:SimplePrefs.SimplePrefsDefaults}
+ * @param {Defaults} [defaults]
+ * @returns {SimplePrefsDefaults}
  */
-
-function simplePrefsDefaults(defaults) {
+function simplePrefsDefaults() {
+  var defaults = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   return new SimplePrefsDefaults({
     defaults: defaults
   });
